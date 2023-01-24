3cd1d8a76f4514a10947607c596dbe93bb8c2b7d
Joey Armstrong <joey@opennetworking.org>
2023-01-24 10:14:02 -0500

    Added an example of default value assignment and string manipulation

---
+++ bash/strings/default_value.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# -----------------------------------------------------------------------
+# Intent:
+#   o Provide a default string value for a variable.
+#   o From string value extract a directory name.
+# -----------------------------------------------------------------------
+
+# ARTIFACT_GLOB=foo/.*
+
+# Variable defaults with path cleanup
+ARTIFACT_GLOB=${ARTIFACT_GLOB:-"release/*"}
+
+artifact_dir="${ARTIFACT_GLOB%/*}"
+declare -p artifact_dir
+
+# [EOF]
+
+    

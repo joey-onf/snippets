f12c2cc1b5fa4c5ac3898aca6c24f61796b0e08e
Joey Armstrong <joey@opennetworking.org>
2023-04-11 16:14:18 -0400

    Added samples of portable strings

---
+++ bash/strings/quoted_comments.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+## -----------------------------------------------------------------------
+## Intent: 
+## -----------------------------------------------------------------------
+
+##----------------##
+##---]  MAIN  [---##
+##----------------##
+
+declare -a comms=()
+declare -a portable=("\#.\#")
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+	machine=Linux
+	comms+=('#.#')
+	;;
+    Darwin*)
+	machine=Mac
+	comms+=("\#.\#")
+	;;
+    CYGWIN*)
+	machine=Cygwin
+	;;
+    MINGW*)
+	machine=MinGw
+	;;
+    *)
+	machine="UNKNOWN:${unameOut}"
+	;;
+esac
+
+echo "Quoted Comment Strings:"
+echo "    Native: $(declare -p comms)"
+echo "  Portable: $(declare -p portable)"
